4021ebb330dcd0128356b20c9e259f334ed241ba
luckylooky2 <luckylooky2@naver.com>
2024-11-14 18:02:59 +0900

    ✨ 퀴즈 상태에 따라 컴포넌트 교체

--- frontend/src/app/quiz/page.tsx
+++ frontend/src/app/quiz/page.tsx
@@ -1,32 +1,57 @@
 "use client";
 
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import { Button } from "@mui/material";
-import { useAtom } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 
-import { quizIsStartedState, QuizStatus } from "@/entities/quiz/model";
+import {
+  quizCurrentKanjiState,
+  quizCurrentRoundState,
+  quizIsStartedState,
+  QuizStatus,
+} from "@/entities/quiz/model";
 import QuizOptionSelect from "@/features/landing/components/QuizOptionSelect";
+import { useModal } from "@/features/modal/hooks/useModal";
 import ToggleRegister from "@/features/myword/components/ToggleRegister";
 import ProgressBar from "@/features/quiz/components/ProgressBar";
+import QuitModal from "@/features/quiz/components/QuitModal";
 import QuizContainer from "@/features/quiz/components/QuizContainer";
+import QuizResult from "@/features/quiz/components/QuizResult";
 
 export default function QuizPage() {
-  const [quizStatus, setQuizStatus] = useAtom(quizIsStartedState);
+  const quizStatus = useAtomValue(quizIsStartedState);
+  const setCurrentRound = useSetAtom(quizCurrentRoundState);
+  const setCurrentKanji = useSetAtom(quizCurrentKanjiState);
+  const { isOpen, setIsOpen } = useModal();
+
+  function resetState() {
+    setCurrentRound(0);
+    setCurrentKanji(null);
+  }
 
   if (quizStatus === QuizStatus.OPTION) {
+    resetState();
     return <QuizOptionSelect />;
   }
+
+  if (quizStatus === QuizStatus.RESULT) {
+    return <QuizResult />;
+  }
+
   return (
     <>
-      <Button
-        variant="text"
-        color="error"
-        onClick={() => {
-          // 모달 적용하기
-          setQuizStatus(QuizStatus.OPTION);
-        }}
-      >
-        Quit
-      </Button>
+      <QuitModal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div>
+        <Button
+          variant="text"
+          color="error"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          <KeyboardDoubleArrowLeftIcon />
+        </Button>
+      </div>
       <ProgressBar />
       <ToggleRegister />
       <QuizContainer />
