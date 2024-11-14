77ddc8c1c7680af997ed5bd96f6151084f767b2c
luckylooky2 <luckylooky2@naver.com>
2024-11-14 16:58:44 +0900

    Feat: 컴포넌트 최초 렌더링 시 호출하는 useAPI

---
+++ frontend/src/shared/hooks/useAPI.ts
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+function useAPI<T>(url: string, options: RequestInit = {}) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+
+        const result = await response.json();
+        if (isMounted) {
+          setData(result);
+        }
+      } catch (err) {
+        if (isMounted) {
+          setError(err.message);
+        }
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [url, options]);
+
+  return { data, loading, error };
+}
+
+export default useAPI;
