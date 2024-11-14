480d34cf51f273c331187cbf3fa9e4f2d8e30ee8
luckylooky2 <luckylooky2@naver.com>
2024-11-14 17:40:02 +0900

    ✨ /quiz/answer API 적용

--- frontend/src/features/quiz/components/AnswerForm.tsx
+++ frontend/src/features/quiz/components/AnswerForm.tsx
@@ -1,6 +1,6 @@
 import Button from "@mui/material/Button";
 import { useSetAtom, useAtomValue } from "jotai";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 import { quizOptionRoundState } from "@/entities/option/store";
@@ -8,34 +8,56 @@ import {
   AnswerInputType,
   quizCurrentRoundState,
   AnswerStatus,
+  quizIsStartedState,
+  QuizStatus,
+  QuizQuestionDTO,
+  quizCurrentKanjiState,
+  QuizAnswerDTO,
 } from "@/entities/quiz/model";
-import { Kanji } from "@/shared/model/db";
 import "../../../app/ui/utils.css";
+import useEventAPI from "@/shared/hooks/useEventAPI";
+import { API_URL, BASE_OPTIONS } from "@/shared/model";
 
 interface Props {
-  kanji: Kanji;
   random: () => void;
 }
 
-const AnswerForm = ({ kanji, random }: Props) => {
+const AnswerForm = ({ random }: Props) => {
   const { register, handleSubmit, reset } = useForm<AnswerInputType>();
   const [, setStatus] = useState(AnswerStatus.BEFORE);
   const setCurrentRound = useSetAtom(quizCurrentRoundState);
+  const setQuizStatus = useSetAtom(quizIsStartedState);
   const maxRound = useAtomValue(quizOptionRoundState);
+  const kanji = useAtomValue<QuizQuestionDTO | null>(quizCurrentKanjiState);
   const [shake, setShake] = useState(false);
   const timeId = useRef<NodeJS.Timeout | null>(null);
+  const { fetchData } = useEventAPI(`${API_URL}/quiz/answer`);
+
+  const getNextQuestion = () => {
+    random();
+    reset();
+    setCurrentRound((prev) => {
+      if (prev + 1 === maxRound) {
+        setQuizStatus(QuizStatus.RESULT);
+      }
+      return prev + 1;
+    });
+  };
 
   // handleSubmit이 data(input 값을) 인자로 호출
-  const onSubmit = ({ answer }: AnswerInputType) => {
-    if (answer === kanji[1]) {
-      random();
-      reset();
-      setCurrentRound((prev) => {
-        if (prev + 1 === maxRound) {
-          console.log(123);
-        }
-        return prev + 1;
-      });
+  const onSubmit = async ({ answer }: AnswerInputType) => {
+    if (!kanji || !answer) {
+      return;
+    }
+
+    const response: QuizAnswerDTO = await fetchData({
+      method: "POST",
+      body: JSON.stringify({ word: kanji.word, answer: answer }),
+      ...BASE_OPTIONS,
+    });
+
+    if (response.result) {
+      getNextQuestion();
     } else {
       triggerShake();
     }
@@ -69,22 +91,10 @@ const AnswerForm = ({ kanji, random }: Props) => {
           autoFocus
         />
       </form>
-      <Button
-        variant="contained"
-        style={{ flex: 1 }}
-        onClick={handleSubmit(onSubmit)}
-      >
+      <Button variant="contained" onClick={handleSubmit(onSubmit)}>
         submit
       </Button>
-      <Button
-        style={{ flex: 1 }}
-        onClick={() => {
-          random();
-          reset();
-        }}
-      >
-        skip
-      </Button>
+      <Button onClick={getNextQuestion}>skip</Button>
     </>
   );
 };
