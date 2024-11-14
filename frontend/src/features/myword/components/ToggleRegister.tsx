02cdb0be81454335301b0264548e541e137b3b80
luckylooky2 <luckylooky2@naver.com>
2024-11-14 17:48:03 +0900

    ✨ 나만의 단어장 추가 버튼에 글로벌 모달 기능 추가

--- frontend/src/features/myword/components/ToggleRegister.tsx
+++ frontend/src/features/myword/components/ToggleRegister.tsx
@@ -3,10 +3,14 @@
 import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
 import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined";
 import IconButton from "@mui/material/IconButton";
+import { useSetAtom } from "jotai";
 import { useState } from "react";
+
+import { GlobalModalIsOpenState } from "@/entities/modal/store";
 
 const ToggleRegister = () => {
   const [isRegistered, setIsRegistered] = useState(false);
+  const setIsOpen = useSetAtom(GlobalModalIsOpenState);
   return (
     <div style={{ display: "flex", justifyContent: "flex-end" }}>
       <IconButton
@@ -14,7 +18,8 @@ const ToggleRegister = () => {
         color="primary"
         style={{ right: "0", width: "40px" }}
         onClick={() => {
-          setIsRegistered((prev) => !prev);
+          setIsOpen(true);
+          // setIsRegistered((prev) => !prev);
         }}
       >
         {isRegistered ? <BookmarkOutlinedIcon /> : <BookmarkAddOutlinedIcon />}
