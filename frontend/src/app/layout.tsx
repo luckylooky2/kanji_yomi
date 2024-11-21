8350aa25d2321082088022ebf754cd509dfddc9c
luckylooky2 <luckylooky2@naver.com>
2024-11-22 00:12:58 +0900

    🐛 Og_image 파일 이름 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     url: "https://kanji-yomi.vercel.app/",
     images: [
       {
-        url: "https://kanji-yomi.vercel.app/og-image.png",
+        url: "https://kanji-yomi.vercel.app/og_image.png",
         width: 1200,
         height: 630,
         alt: "Kanji Yomi Open Graph Image",
