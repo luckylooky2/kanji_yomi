74366d956118b2ec46d3068da1abf66baf42e3fc
luckylooky2 <luckylooky2@naver.com>
2024-11-21 15:29:48 +0900

    🚨 Eslint 사용하지 않은 변수 주석 처리

--- frontend/src/widgets/navigation/PageWithBottomNav.tsx
+++ frontend/src/widgets/navigation/PageWithBottomNav.tsx
@@ -1,50 +1,49 @@
 import styled from "@emotion/styled";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import ForumIcon from "@mui/icons-material/Forum";
-import QuizIcon from "@mui/icons-material/Quiz";
-import Navigation from "@mui/material/BottomNavigation";
-import NavigationAction from "@mui/material/BottomNavigationAction";
-import Link from "next/link";
-import React, { useCallback } from "react";
+// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+// import ForumIcon from "@mui/icons-material/Forum";
+// import QuizIcon from "@mui/icons-material/Quiz";
+// import Navigation from "@mui/material/BottomNavigation";
+// import NavigationAction from "@mui/material/BottomNavigationAction";
+// import Link from "next/link";
+// import React, { useCallback } from "react";
 
-import { getNavigationStatus } from "@/shared/model";
+// import { getNavigationStatus } from "@/shared/model";
 
 interface Props {
   children: React.ReactNode;
   path: string;
 }
 
-/* eslint-disable no-unused-vars */
-const BottomNavigation = ({ path }: { path: string }) => {
-  const handleClick = useCallback(
-    ({ currentTarget }: { currentTarget: HTMLButtonElement }) => {
-      const link = currentTarget.querySelector("a");
-      if (link) {
-        link.click();
-      }
-    },
-    []
-  );
+// const BottomNavigation = ({ path }: { path: string }) => {
+//   const handleClick = useCallback(
+//     ({ currentTarget }: { currentTarget: HTMLButtonElement }) => {
+//       const link = currentTarget.querySelector("a");
+//       if (link) {
+//         link.click();
+//       }
+//     },
+//     []
+//   );
 
-  const menus = [
-    { label: "Quiz", href: "quiz", icon: <QuizIcon /> },
-    { label: "Community", href: "community", icon: <ForumIcon /> },
-    { label: "My Page", href: "mypage", icon: <AccountCircleIcon /> },
-  ];
+//   const menus = [
+//     { label: "Quiz", href: "quiz", icon: <QuizIcon /> },
+//     { label: "Community", href: "community", icon: <ForumIcon /> },
+//     { label: "My Page", href: "mypage", icon: <AccountCircleIcon /> },
+//   ];
 
-  return (
-    <Navigation value={getNavigationStatus(path)} component="nav">
-      {menus.map((menu, index) => (
-        <NavigationAction
-          key={index}
-          label={menu.label}
-          icon={<Link href={menu.href}>{menu.icon}</Link>}
-          onClick={handleClick}
-        />
-      ))}
-    </Navigation>
-  );
-};
+//   return (
+//     <Navigation value={getNavigationStatus(path)} component="nav">
+//       {menus.map((menu, index) => (
+//         <NavigationAction
+//           key={index}
+//           label={menu.label}
+//           icon={<Link href={menu.href}>{menu.icon}</Link>}
+//           onClick={handleClick}
+//         />
+//       ))}
+//     </Navigation>
+//   );
+// };
 
 const PageWithBottomNav = ({ children }: Props) => {
   return (
