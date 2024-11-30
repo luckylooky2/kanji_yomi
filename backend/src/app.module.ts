1cbc592840feea4707f007774a2bcafd287678b7
luckylooky2 <luckylooky2@naver.com>
2024-11-30 11:03:59 +0900

    🔥 [BE] 필요없는 모듈 제거

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { UsersModule } from './users/user.module';
 import { Question } from './question/question.entity';
 import { QuestionModule } from './question/question.module';
 import 'dotenv/config';
@@ -15,11 +13,10 @@ import 'dotenv/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [Question, User],
+      entities: [Question],
       synchronize: true,
       autoLoadEntities: true,
     }),
-    UsersModule,
     QuestionModule,
   ],
 })
