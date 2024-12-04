8bc8f600daacfde4ebda50aead0825f2ccda0c8e
luckylooky2 <luckylooky2@naver.com>
2024-12-04 17:41:28 +0900

    🐛 2자리 언어코드를 4자리로 변경

--- frontend/src/entities/quiz/lib/playTTS.ts
+++ frontend/src/entities/quiz/lib/playTTS.ts
@@ -2,7 +2,7 @@ export const playTTS = (word: string) => {
   const synth = window.speechSynthesis;
   const utterThis = new SpeechSynthesisUtterance(word);
   // TODO: 값 변경 UI 만들기
-  utterThis.lang = "ja";
+  utterThis.lang = "ja-JP";
   utterThis.rate = 0.5;
 
   synth.speak(utterThis);
