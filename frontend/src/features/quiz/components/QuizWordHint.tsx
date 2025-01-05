3aa393bea52afce7701df3619c17bd1dfef550f6
luckylooky2 <luckylooky2@naver.com>
2025-01-05 23:44:44 +0900

    💄 QuizWordHint에 Popper를 추가하여 on/off 기능 추가

--- frontend/src/features/quiz/components/QuizWordHint.tsx
+++ frontend/src/features/quiz/components/QuizWordHint.tsx
@@ -1,14 +1,19 @@
 import styled from "@emotion/styled";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import TravelExploreIcon from "@mui/icons-material/TravelExplore";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
-import { Button, ButtonGroup } from "@mui/material";
+import { Button, ButtonGroup, Popper } from "@mui/material";
 import { useAtom } from "jotai";
+import React, { useState } from "react";
 
 import { playTTS } from "@/entities/quiz/lib/playTTS";
 import { quizCurrentKanjiState } from "@/entities/quiz/store";
 
 const QuizWordHint = () => {
   const [{ data: kanji }] = useAtom(quizCurrentKanjiState);
+  const [isOpen, setIsOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleSpeakWord = () => {
     if (!kanji) {
@@ -24,16 +29,30 @@ const QuizWordHint = () => {
     window.open(`https://jisho.org/search/${kanji.word}`, "_blank");
   };
 
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+    setIsOpen((previousOpen) => !previousOpen);
+  };
+
   return (
     <QuizWordHintLayout>
-      <ButtonGroup variant="outlined" aria-label="Hint button group">
-        <Button onClick={handleSpeakWord}>
-          <VolumeUpIcon />
-        </Button>
-        <Button onClick={handleRedirectDictionary}>
-          <TravelExploreIcon />
-        </Button>
-      </ButtonGroup>
+      <Button variant="text" onClick={handleClick}>
+        {isOpen ? <RemoveIcon /> : <AddIcon />}
+      </Button>
+      <Popper open={isOpen} anchorEl={anchorEl} placement="top">
+        <QuizButtonGroup
+          variant="outlined"
+          orientation="vertical"
+          aria-label="Hint button group"
+        >
+          <Button onClick={handleSpeakWord}>
+            <VolumeUpIcon />
+          </Button>
+          <Button onClick={handleRedirectDictionary}>
+            <TravelExploreIcon />
+          </Button>
+        </QuizButtonGroup>
+      </Popper>
     </QuizWordHintLayout>
   );
 };
@@ -43,5 +62,8 @@ export default QuizWordHint;
 const QuizWordHintLayout = styled.div`
   display: flex;
   justify-content: center;
-  height: 30px;
+`;
+
+const QuizButtonGroup = styled(ButtonGroup)`
+  margin-bottom: 10px;
 `;
