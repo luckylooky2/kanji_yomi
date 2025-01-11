289c590e12393a1705f8174214562082aca9cc5d
luckylooky2 <luckylooky2@naver.com>
2025-01-12 00:03:04 +0900

    👔 #12 TTS 관련 함수를 컴포넌트로 통합, speakSetting 토글 추가

--- frontend/src/features/quiz/components/QuizWordHint.tsx
+++ frontend/src/features/quiz/components/QuizWordHint.tsx
@@ -2,25 +2,62 @@ import styled from "@emotion/styled";
 import ClearIcon from "@mui/icons-material/Clear";
 import MenuIcon from "@mui/icons-material/Menu";
 import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import TuneIcon from "@mui/icons-material/Tune";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import { Button, ButtonGroup, Popper } from "@mui/material";
 import { useAtom } from "jotai";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { playTTS } from "@/entities/quiz/lib/playTTS";
-import { quizCurrentKanjiState } from "@/entities/quiz/store";
+import {
+  defaultSpeakSetting,
+  playTTS,
+  SSVLangJapanese,
+} from "@/entities/quiz/lib";
+import {
+  quizCurrentKanjiState,
+  quizHintSpeakSettingState,
+  quizHintVoiceListState,
+} from "@/entities/quiz/store";
 import ResponsiveIcon from "@/widgets/ResponsiveIcon/ResponsiveIcon";
+
+import QuizWordHintSpeakSetting from "./QuizWordHintSpeakSetting";
 
 const QuizWordHint = () => {
   const [{ data: kanji }] = useAtom(quizCurrentKanjiState);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [isSpeakSettingOpen, setIsSpeakSettingOpen] = useState(false);
+  const [speakSetting, setSpeakSetting] = useAtom(quizHintSpeakSettingState);
+  const [voiceList, setVoiceList] = useAtom(quizHintVoiceListState);
+
+  const resetSpeakSetting = () => {
+    setSpeakSetting(defaultSpeakSetting);
+    loadVoices();
+  };
+
+  const loadVoices = (onComplete = () => {}) => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const allVoices = window.speechSynthesis.getVoices();
+    const voices = allVoices.filter((voice) => voice.lang === SSVLangJapanese);
+
+    if (voices.length !== 0) {
+      setVoiceList(voices);
+      onComplete();
+    } else {
+      return setTimeout(function () {
+        loadVoices(onComplete);
+      }, 100);
+    }
+  };
 
   const handleSpeakWord = () => {
     if (!kanji) {
       return;
     }
-    playTTS(kanji.word);
+    playTTS(speakSetting, voiceList, kanji.word, resetSpeakSetting);
   };
 
   const handleRedirectDictionary = () => {
@@ -32,15 +69,20 @@ const QuizWordHint = () => {
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
-    setIsOpen((previousOpen) => !previousOpen);
+    setIsMenuOpen((previousOpen) => !previousOpen);
+    setIsSpeakSettingOpen(false);
   };
+
+  useEffect(() => {
+    loadVoices();
+  }, []);
 
   return (
     <QuizWordHintLayout>
       <QuizWordHintMenuButton variant="text" onClick={handleClick}>
-        <ResponsiveIcon icon={isOpen ? ClearIcon : MenuIcon} />
+        <ResponsiveIcon icon={isMenuOpen ? ClearIcon : MenuIcon} />
       </QuizWordHintMenuButton>
-      <Popper open={isOpen} anchorEl={anchorEl} placement="bottom">
+      <Popper open={isMenuOpen} anchorEl={anchorEl} placement="bottom">
         <QuizWordHintButtonGroup
           variant="outlined"
           orientation="vertical"
@@ -49,6 +91,13 @@ const QuizWordHint = () => {
           <QuizWordHintButton onClick={handleSpeakWord}>
             <ResponsiveIcon icon={VolumeUpIcon} />
           </QuizWordHintButton>
+          <QuizWordHintButton
+            onClick={() => setIsSpeakSettingOpen(!isSpeakSettingOpen)}
+            variant={isSpeakSettingOpen ? "contained" : "outlined"}
+          >
+            <ResponsiveIcon icon={TuneIcon} />
+          </QuizWordHintButton>
+          {isSpeakSettingOpen && <QuizWordHintSpeakSetting />}
           <QuizWordHintButton onClick={handleRedirectDictionary}>
             <ResponsiveIcon icon={TravelExploreIcon} />
           </QuizWordHintButton>
@@ -66,13 +115,12 @@ const QuizWordHintLayout = styled.div`
 `;
 
 const QuizWordHintButtonGroup = styled(ButtonGroup)`
-  margin-top: 10px;
+  position: relative;
+  margin-top: 5px;
 `;
 
 const QuizWordHintMenuButton = styled(Button)`
   padding: 0;
 `;
 
-const QuizWordHintButton = styled(Button)`
-  background-color: white;
-`;
+const QuizWordHintButton = styled(Button)``;
