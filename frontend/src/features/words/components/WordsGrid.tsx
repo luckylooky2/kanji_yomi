4cd360048972c941bf9e870aaf5bf2d84fa382e3
luckylooky2 <luckylooky2@naver.com>
2025-01-21 03:05:21 +0900

    ✨ #18 검색한 단어를 보여주는 그리드 컴포넌트

---
+++ frontend/src/features/words/components/WordsGrid.tsx
@@ -0,0 +1,95 @@
+import styled from "@emotion/styled";
+import Grid from "@mui/material/Grid2";
+import { useAtom } from "jotai";
+import { useEffect, useRef } from "react";
+
+import { wordsCurrentWordIndex } from "@/entities/words/store";
+import { useMediaQuery } from "@/shared/hooks/useMediaQuery";
+import { theme } from "@/shared/styles/theme";
+import { WordInfo } from "@/shared/types";
+
+import WordsCurrentWord from "./WordsCurrentWord";
+
+interface Props {
+  words: WordInfo[];
+}
+
+const WordsGrid = ({ words }: Props) => {
+  const isMobile = useMediaQuery(theme.breakpoints.mobile);
+  const [currentWordIndex, setCurrentWordIndex] = useAtom(
+    wordsCurrentWordIndex
+  );
+  const itemRefs = useRef<HTMLDivElement[]>([]);
+  const isWordSelected = currentWordIndex !== null;
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isWordSelected && itemRefs.current[currentWordIndex]) {
+        itemRefs.current[currentWordIndex].scrollIntoView({
+          behavior: "smooth",
+          block: "nearest",
+        });
+      }
+    }, 200);
+  }, [currentWordIndex]);
+
+  return (
+    <>
+      <WordsSearchResultGrid
+        container
+        spacing={2}
+        columns={24}
+        isWordSelected={isWordSelected}
+      >
+        {words.map((item, index) => (
+          <Item
+            size={isMobile ? 12 : 8}
+            key={item.id}
+            ref={(el: HTMLDivElement) => {
+              if (itemRefs.current) {
+                itemRefs.current[index] = el;
+              }
+            }}
+            isActive={currentWordIndex === item.id}
+            onClick={() => {
+              const nextIndex = index === currentWordIndex ? null : index;
+              setCurrentWordIndex(nextIndex);
+              setTimeout(() => {}, 1000);
+            }}
+          >
+            <h3>{item.word}</h3>
+          </Item>
+        ))}
+      </WordsSearchResultGrid>
+      {isWordSelected && <WordsCurrentWord word={words[currentWordIndex]} />}
+    </>
+  );
+};
+
+export default WordsGrid;
+
+const Item = styled(Grid)<{ isActive?: boolean }>`
+  background: #fff;
+  border: 1px solid #ddd;
+  border-style: solid;
+  border-radius: 8px;
+  background-color: ${({ isActive }) =>
+    isActive ? "rgba(25, 118, 210, 0.5)" : "white"};
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 16px;
+  transition: transform 0.2s ease, box-shadow 0.2s ease;
+  font-weight: bold;
+
+  h3 {
+    text-align: center;
+  }
+`;
+
+const WordsSearchResultGrid = styled(Grid)<{ isWordSelected: boolean }>`
+  overflow: auto;
+  height: ${({ isWordSelected }) =>
+    isWordSelected ? "calc(90% - 200px)" : "90%"};
+  padding: ${theme.spacing.medium};
+  scroll-behavior: smooth;
+  transition: height 0.1s ease-out;
+`;
