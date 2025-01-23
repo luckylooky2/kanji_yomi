88ef44d6539c28fbce2ba4908791cf8760b76b32
luckylooky2 <luckylooky2@naver.com>
2025-01-24 01:31:56 +0900

    🦺 #19 offset, limit regex 검증 추가

--- backend/src/question/words.request.ts
+++ backend/src/question/words.request.ts
@@ -1,12 +1,24 @@
+import { BadRequestException } from '@nestjs/common';
 import { Transform } from 'class-transformer';
 import {
   IsArray,
   IsIn,
+  IsInt,
   IsNumber,
   IsOptional,
   IsString,
+  Max,
   Min,
 } from 'class-validator';
+
+function checkNumberRegex({ value }: { value: string }) {
+  const regex = /^\d+$/;
+  if (regex.test(value)) {
+    return parseInt(value, 10);
+  }
+
+  throw new BadRequestException('Invalid number format');
+}
 
 export class WordsQueryRequest {
   @IsOptional()
@@ -27,13 +39,17 @@ export class WordsQueryRequest {
 
   @IsOptional()
   @IsNumber()
+  @IsInt()
   @Min(0)
-  @Transform(({ value }) => parseInt(value, 10))
+  @Max(1_000_000)
+  @Transform(checkNumberRegex)
   offset?: number = 0;
 
   @IsOptional()
   @IsNumber()
   @Min(1)
-  @Transform(({ value }) => parseInt(value, 10))
+  @Max(1_000_000)
+  @IsInt()
+  @Transform(checkNumberRegex)
   limit?: number = 50;
 }
