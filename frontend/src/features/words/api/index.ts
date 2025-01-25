9f88a5165bb5a8c3b0781de877762dfa3c6d0fda
luckylooky2 <luckylooky2@naver.com>
2025-01-25 13:54:54 +0900

    👽️ #20 /words/count API 함수 추가

--- frontend/src/features/words/api/index.ts
+++ frontend/src/features/words/api/index.ts
@@ -4,6 +4,39 @@ import {
 } from "@/entities/words/types";
 import { API_URL, responseInterceptor } from "@/shared/model";
 
+function createQueryStringWithFilters(
+  search: string,
+  difficulty: Record<WordsSearchFilterDifficultyType, boolean>,
+  correctRatio: Record<WordsSearchFilterCorrectRatioType, boolean>,
+  pageParams: number | null = null
+) {
+  const qs = new URLSearchParams();
+  if (search.trim()) {
+    qs.append("search", search.trim());
+  }
+  for (const key in difficulty) {
+    if (difficulty[key as WordsSearchFilterDifficultyType]) {
+      if (key === "All") {
+        continue;
+      }
+      qs.append("difficulty", key);
+    }
+  }
+  for (const key in correctRatio) {
+    if (correctRatio[key as WordsSearchFilterCorrectRatioType]) {
+      if (key === "All") {
+        continue;
+      }
+      qs.append("correctRatio", key);
+    }
+  }
+  if (pageParams !== null) {
+    qs.append("offset", String(pageParams));
+  }
+
+  return qs.toString() ? `?${qs.toString()}` : "";
+}
+
 export const WordsService = {
   searchWords: async (
     search: string,
@@ -11,30 +44,27 @@ export const WordsService = {
     correctRatio: Record<WordsSearchFilterCorrectRatioType, boolean>,
     pageParams: number
   ) => {
-    const qs = new URLSearchParams();
-    if (search.trim()) {
-      qs.append("search", search.trim());
-    }
-    for (const key in difficulty) {
-      if (difficulty[key as WordsSearchFilterDifficultyType]) {
-        if (key === "All") {
-          continue;
-        }
-        qs.append("difficulty", key);
-      }
-    }
-    for (const key in correctRatio) {
-      if (correctRatio[key as WordsSearchFilterCorrectRatioType]) {
-        if (key === "All") {
-          continue;
-        }
-        qs.append("correctRatio", key);
-      }
-    }
-    qs.append("offset", pageParams + "");
-
-    const queryString = qs.toString() ? `?${qs.toString()}` : "";
-
+    const queryString = createQueryStringWithFilters(
+      search,
+      difficulty,
+      correctRatio,
+      pageParams
+    );
     return responseInterceptor(await fetch(`${API_URL}/words${queryString}`));
+  },
+
+  searchWordsCount: async (
+    search: string,
+    difficulty: Record<WordsSearchFilterDifficultyType, boolean>,
+    correctRatio: Record<WordsSearchFilterCorrectRatioType, boolean>
+  ) => {
+    const queryString = createQueryStringWithFilters(
+      search,
+      difficulty,
+      correctRatio
+    );
+    return responseInterceptor(
+      await fetch(`${API_URL}/words/count${queryString}`)
+    );
   },
 };
