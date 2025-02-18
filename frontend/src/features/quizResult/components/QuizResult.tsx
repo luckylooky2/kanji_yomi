e92ccd3954ba22a7e0bc0bf305cc74ad3dc93bd0
luckylooky2 <luckylooky2@naver.com>
2025-02-18 10:45:25 +0900

    🎨 #26 uuid 길이 축소

--- frontend/src/features/quizResult/components/QuizResult.tsx
+++ frontend/src/features/quizResult/components/QuizResult.tsx
@@ -93,7 +93,7 @@ const QuizResult = () => {
     const blob = new Blob([csvHeader + csvContent], {
       type: "application/octet-stream;charset=utf-8;",
     });
-    const uuid = crypto.randomUUID().slice(0, 8);
+    const uuid = crypto.randomUUID().slice(0, 6);
     const filename = `kanjiyomi_${formattedTimestamp}_${difficulty}_${uuid}.csv`;
 
     saveAs(blob, filename);
