5ae52167d7af730dbe6f13aac042a776e01e620f
luckylooky2 <luckylooky2@naver.com>
2025-02-27 18:07:42 +0900

    🔍️ #30 setting 페이지 메타데이터 추가

--- frontend/src/app/setting/layout.tsx
+++ frontend/src/app/setting/layout.tsx
@@ -1,24 +1,24 @@
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
 import MobileLayout from "@/widgets/MobileLayout/MobileLayout";
 
-// export const metadata: Metadata = {
-//   title: "Words - Kanji Yomi",
-//   description: "Search the Kanji words from the quiz",
-//   openGraph: {
-//     title: "Words - Kanji Yomi",
-//     description: "Search the Kanji words from the quiz",
-//     url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/words`,
-//     images: [
-//       {
-//         url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/og_image.png`,
-//         width: 1200,
-//         height: 630,
-//         alt: "Kanji Yomi Open Graph Image",
-//       },
-//     ],
-//     type: "website",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Setting - Kanji Yomi",
+  description: "Manage language and other preferences",
+  openGraph: {
+    title: "Setting - Kanji Yomi",
+    description: "Manage language and other preferences",
+    url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/words`,
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/og_image.png`,
+        width: 1200,
+        height: 630,
+        alt: "Kanji Yomi Open Graph Image",
+      },
+    ],
+    type: "website",
+  },
+};
 
 export default MobileLayout;
