0170e9bdd4ef47aa9567152f6ff58f83716be271
luckylooky2 <luckylooky2@naver.com>
2025-03-01 21:36:06 +0900

    🔥 #31 jotai 기반 API fetch 코드 삭제

--- frontend/src/entities/quiz/store/index.ts
+++ frontend/src/entities/quiz/store/index.ts
@@ -1,17 +1,12 @@
 import { atom } from "jotai";
-import { atomWithRefresh, atomWithStorage } from "jotai/utils";
-
-import { quizOptionDifficultyState } from "@/entities/quizOption/store";
-import { QuizService } from "@/features/quiz/api";
+import { atomWithStorage } from "jotai/utils";
 
 import {
   validateShowQuizUserGuideAtom,
   validateSpeakSettingAtom,
 } from "../lib";
 import {
-  QuizAnswerResponseDTO,
   QuizHintSpeakSetting,
-  QuizQuestionResponseDTO,
   QuizStatus,
   QuizTimer,
   QuizUserGuideStepType,
@@ -21,40 +16,11 @@ import {
 export const quizStatusState = atom<QuizStatus>(QuizStatus.OPTION);
 export const quizCurrentRoundState = atom(0);
 export const quizCurrentRetries = atom(0);
-export const quizCurrentKanjiState = atomWithRefresh(async (get) => {
-  get(quizCurrentRoundState);
-  const difficulty = get(quizOptionDifficultyState);
-  try {
-    const data: QuizQuestionResponseDTO = await QuizService.getQuestion({
-      difficulty,
-    });
-    return { data };
-  } catch {
-    return { error: { message: "Network Error" } };
-  }
-});
-
+export const quizAnswerInputState = atom("");
 export const quizTimerState = atom<QuizTimer>({
   quizStartTime: null,
   quizEndTime: null,
 });
-
-export const quizAnswerResultState = atom(
-  { data: null },
-  async (get, _set, ...args) => {
-    const { data: kanji } = await get(quizCurrentKanjiState);
-    try {
-      const data: QuizAnswerResponseDTO = await QuizService.getAnswer({
-        id: kanji!.id,
-        word: kanji!.word,
-        answer: args[0] as string,
-      });
-      return { data };
-    } catch {
-      return { error: { message: "Network Error" } };
-    }
-  }
-);
 
 // QuizHint
 export const quizHintVoiceListState = atom<SpeechSynthesisVoice[]>([]);
