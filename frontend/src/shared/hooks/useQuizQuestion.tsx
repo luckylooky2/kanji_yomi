dcf1928fcfd3c52eb5a2339a0a1d2d21fea2d131
luckylooky2 <luckylooky2@naver.com>
2025-03-01 21:37:09 +0900

    👔 #31 quiz/question API 리액트 쿼리 적용 및 사용자 정의 훅 생성

---
+++ frontend/src/shared/hooks/useQuizQuestion.tsx
@@ -0,0 +1,29 @@
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import { useAtom } from "jotai";
+
+import { quizCurrentRoundState, quizTimerState } from "@/entities/quiz/store";
+import { quizOptionDifficultyState } from "@/entities/quizOption/store";
+import { QuizService } from "@/features/quiz/api";
+
+import { WordInfo } from "../types";
+
+export const useQuizQuestion = () => {
+  const [difficulty] = useAtom(quizOptionDifficultyState);
+  const [currentRound] = useAtom(quizCurrentRoundState);
+  const [{ quizStartTime }] = useAtom(quizTimerState);
+
+  const queryFn = async () => {
+    try {
+      return QuizService.getQuestion({ difficulty });
+    } catch {
+      throw new Error("Network Error");
+    }
+  };
+
+  return useQuery<WordInfo>({
+    queryKey: ["quizQuestion", currentRound, quizStartTime],
+    queryFn,
+    staleTime: Infinity,
+    placeholderData: keepPreviousData,
+  });
+};
