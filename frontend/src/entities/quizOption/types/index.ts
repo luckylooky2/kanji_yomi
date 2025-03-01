43ed1d5e2429c86853da6567267ac163fb32de61
luckylooky2 <luckylooky2@naver.com>
2025-03-01 21:34:52 +0900

    🏷️ #31 난이도 타입 추가

--- frontend/src/entities/quizOption/types/index.ts
+++ frontend/src/entities/quizOption/types/index.ts
@@ -1,4 +1,6 @@
 export type QuizOption = {
-  difficulty: string[];
+  difficulty: QuizOptionDifficulty[];
   round: number;
 };
+
+export type QuizOptionDifficulty = "N1" | "N2" | "N3" | "N4" | "N5";
