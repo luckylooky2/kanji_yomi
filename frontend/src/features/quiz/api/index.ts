dfe84075ab422cbd7a06231fa3e0aafd11d5db0e
luckylooky2 <luckylooky2@naver.com>
2025-03-04 23:22:59 +0900

    👔 #33 quiz/start, finish API 추가

--- frontend/src/features/quiz/api/index.ts
+++ frontend/src/features/quiz/api/index.ts
@@ -1,10 +1,30 @@
 import {
   QuizAnswerRequestDTO,
   QuizQuestionRequestDTO,
+  QuizStartRequestDTO,
 } from "@/entities/quiz/types";
 import { API_URL, BASE_OPTIONS, responseInterceptor } from "@/shared/model";
 
 export const QuizService = {
+  startQuiz: async (body: QuizStartRequestDTO) => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(body),
+      ...BASE_OPTIONS,
+    };
+
+    return responseInterceptor(`${API_URL}/quiz/start`, options);
+  },
+
+  finishQuiz: async (id: string) => {
+    const options = {
+      method: "PATCH",
+      ...BASE_OPTIONS,
+    };
+
+    return responseInterceptor(`${API_URL}/quiz/finish/${id}`, options);
+  },
+
   getQuestion: async (body: QuizQuestionRequestDTO) => {
     const options = {
       method: "POST",
