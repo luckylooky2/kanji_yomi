2fbbbeec830e2f0887e8788b50ce74a7235a2cb4
luckylooky2 <luckylooky2@naver.com>
2025-03-05 18:36:57 +0900

    ♻️ #34 변수 이름 수정

--- frontend/src/shared/hooks/useDelayFetching.ts
+++ frontend/src/shared/hooks/useDelayFetching.ts
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 
-export function useDelayFetching(state: boolean) {
+export function useDelayFetching(isFetching: boolean) {
   const [delay, setDelay] = useState(false);
+  const delayTime = 300;
 
   useEffect(() => {
-    if (state) {
-      const timer = setTimeout(() => setDelay(true), 300);
+    if (isFetching) {
+      const timer = setTimeout(() => setDelay(true), delayTime);
+
       return () => {
         setDelay(false);
         clearTimeout(timer);
       };
     }
-  }, [state]);
+  }, [isFetching]);
 
-  // state는 미리 바뀌어도 delay가 바뀔 때까지 기다림
-  return delay && state;
+  // isFetching는 미리 바뀌어도 delay가 바뀔 때까지 기다림
+  return delay && isFetching;
 }
