f3935668289828b1050219410ea92d0eb62cc9b3
luckylooky2 <luckylooky2@naver.com>
2025-03-10 16:15:32 +0900

    👔 #35 단어 CRUD 서비스 로직 추가

--- backend/src/words/words.service.ts
+++ backend/src/words/words.service.ts
@@ -1,17 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Word } from '../entity/word.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Brackets, Repository } from 'typeorm';
-import { WordsQueryCountDTO, WordsQueryDto } from './words.dto';
+import {
+  WordsCreateDTO,
+  WordsDeleteDTO,
+  WordsEditDTO,
+  WordsQueryCountDTO,
+  WordsQueryDto,
+} from './words.dto';
 import { plainToInstance } from 'class-transformer';
-import { WordsQueryRequest } from './words.request';
-import { ratioCondition } from 'src/utils/utils';
+import {
+  WordsCreateRequest,
+  WordsEditRequest,
+  WordsQueryRequest,
+} from './words.request';
+import { makeQuestionDTO, ratioCondition } from 'src/utils/utils';
+import { QuestionDTO } from 'src/quiz/quiz.dto';
+import { Meaning } from 'src/entity/meaning.entity';
 
 @Injectable()
 export class WordsService {
   constructor(
     @InjectRepository(Word) private wordRepository: Repository<Word>,
+    @InjectRepository(Meaning) private meaningRepository: Repository<Meaning>,
   ) {}
+
+  private async joinAndSelectById(id: number) {
+    return await this.wordRepository
+      .createQueryBuilder('word')
+      .innerJoinAndSelect('word.meanings', 'meaning')
+      .where('word.id = :id', { id: id })
+      .getOne();
+  }
 
   private getQueryBuilderByFilter(query: WordsQueryRequest) {
     return this.wordRepository
@@ -115,5 +136,103 @@ export class WordsService {
       { totalCount },
       { excludeExtraneousValues: true },
     );
+  }
+
+  async getWordById(questionId: number): Promise<QuestionDTO> {
+    const wordById = await this.joinAndSelectById(questionId);
+
+    if (!wordById) {
+      throw new BadRequestException('No matching meaning found');
+    }
+
+    const questionDAO = makeQuestionDTO(wordById);
+
+    return plainToInstance(QuestionDTO, questionDAO, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async createNewWord(word: WordsCreateRequest): Promise<WordsCreateDTO> {
+    const existingWord = await this.wordRepository.findOne({
+      where: { word: word.word.normalize('NFC') },
+    });
+
+    if (existingWord) {
+      throw new BadRequestException('Word already exists');
+    }
+
+    const newWord = this.wordRepository.create(word);
+    // NFC 정규화를 하지 않아도 answer 로직에서 정규화하여 비교
+    newWord.word = newWord.word.normalize('NFC');
+    // Word 테이블 뿐만 아니라 Meaning 테이블에도 엔티티가 자동으로 추가됨
+    await this.wordRepository.save(newWord);
+
+    return { id: newWord.id };
+  }
+
+  async editWord(
+    wordId: number,
+    newWord: WordsEditRequest,
+  ): Promise<WordsEditDTO> {
+    const existingWord = await this.joinAndSelectById(wordId);
+
+    if (!existingWord) {
+      throw new BadRequestException('No matching word found');
+    }
+
+    const exMeanings = existingWord.meanings
+      .slice()
+      .sort((a, b) => a.meaning.localeCompare(b.meaning));
+    const newMeanings = newWord.meanings
+      .slice()
+      .sort((a, b) => a.meaning.localeCompare(b.meaning));
+
+    const isMeaningChanged =
+      newMeanings.length !== exMeanings.length ||
+      !newMeanings.every(
+        (meaning, index) =>
+          meaning.meaning === exMeanings[index].meaning &&
+          meaning.difficulty === exMeanings[index].difficulty,
+      );
+    const isWordChanged = newWord.word !== existingWord.word;
+
+    // 의미 정보 업데이트
+    if (isMeaningChanged) {
+      await this.meaningRepository.delete({ word: { id: wordId } });
+
+      const newMeanings = newWord.meanings.map((meaning) =>
+        this.meaningRepository.create({
+          ...meaning,
+          word: existingWord, // 관계 설정
+        }),
+      );
+
+      await this.meaningRepository.save(newMeanings);
+      // 없으면 단어와 의미 둘 다 변경이 되었을 때, wordId(FK)가 NULL로 설정됨
+      existingWord.meanings = newMeanings;
+    }
+
+    // 단어 정보 업데이트
+    if (isWordChanged) {
+      existingWord.word = newWord.word;
+      // 없으면 단어 변경 사항이 저장이 되지 않음
+      await this.wordRepository.save(existingWord);
+    }
+
+    return { id: wordId };
+  }
+
+  async deleteWord(wordId: number): Promise<WordsDeleteDTO> {
+    const existingWord = await this.wordRepository.findOne({
+      where: { id: wordId },
+    });
+
+    if (!existingWord) {
+      throw new BadRequestException('No matching word found');
+    }
+
+    await this.wordRepository.delete({ id: wordId });
+
+    return { id: wordId };
   }
 }
