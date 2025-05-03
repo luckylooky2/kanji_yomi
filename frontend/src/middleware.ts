d5ee9ff6bde603e908dc2fbd3b0fff68f648ced1
luckylooky2 <luckylooky2@naver.com>
2025-05-03 17:47:05 +0900

    🎨  #39 불필요 리다이렉트 코드 삭제 및 토큰 검증 로직 추가

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -5,13 +5,6 @@ export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   requestHeaders.set("x-pathname", pathname);
 
-  if (pathname.startsWith("/admin") && pathname !== "/admin/login") {
-    const token = request.cookies.get("access_token");
-    if (!token) {
-      return NextResponse.redirect(new URL("/admin/login", request.url));
-    }
-  }
-
   return NextResponse.next({
     request: {
       headers: requestHeaders,
