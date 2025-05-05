721cd9c530b240207b68246ba654b9795f3889b1
luckylooky2 <luckylooky2@naver.com>
2025-05-05 16:27:40 +0900

    🔍️ Admin 페이지 메타데이터 추가

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,8 +1,14 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Metadata } from "next";
 import { ReactNode } from "react";
 
 import ReactQueryProviders from "@/ReactQueryProviders";
 import Header from "@/features/header/components/Header";
+
+export const metadata: Metadata = {
+  title: "Admin - Kanji Yomi",
+  description: "Admin page",
+};
 
 export default async function AdminLayout({
   children,
